a9c4e48b7c876ecad8a96efd4d2a6e707583153d
babakhadady <107441588+babakhadady@users.noreply.github.com>
2022-12-19 00:16:47 -0800

    Delete project_y9m6h-master directory

--- project_y9m6h-master/src/main/persistence/Writable.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-// Code is based from JSONSerializationDemo
-public interface Writable {
-    // EFFECTS: returns this as a JSON object
-    JSONObject toJson();
-}
--- project_y9m6h-master/src/main/ui/Main.java
+++
@@ -1,9 +0,0 @@
-package ui;
-
-// runs the flashcardAppUI class
-public class Main {
-    public static void main(String[] args) {
-        FlashCardApp flashCardApp = new FlashCardApp();
-    }
-}
-
